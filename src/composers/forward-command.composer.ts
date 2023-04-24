a61a4ca2e7c1c1ec6b7fac97c52949484426f64d
Kuromai <arconiel456@gmail.com>
2023-04-24 19:19:52 +0300

    feat: add forward-command

--- src/composers/forward-command.composer.ts
+++ src/composers/forward-command.composer.ts
@@ -1,3 +1,12 @@
 import { Composer } from 'grammy';
+import { environmentConfig } from '../config';
 
 export const forwardCommandComposer = new Composer();
+
+forwardCommandComposer.command('forward',  async (context)=> {
+    if(context.message?.reply_to_message?.text === undefined){
+       return context.reply('Ви не виділили повідомлення');
+    } else {
+        return await context.api.forwardMessage(environmentConfig.CHANNEL_ID, context.chat.id, context.msg.reply_to_message?.message_id || 0);
+    }
+})
