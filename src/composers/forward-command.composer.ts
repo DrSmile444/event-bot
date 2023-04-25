d488f19104b1544cfc996842b28e1bc5112ef419
Kuromai <arconiel456@gmail.com>
2023-04-25 21:47:34 +0300

    fix: Changing the structure of the if code blok

--- src/composers/forward-command.composer.ts
+++ src/composers/forward-command.composer.ts
@@ -4,9 +4,8 @@ import { environmentConfig } from '../config';
 export const forwardCommandComposer = new Composer();
 
 forwardCommandComposer.command('forward',  async (context)=> {
-    if(context.message?.reply_to_message?.text === undefined){
-       return context.reply('Ви не виділили повідомлення');
-    } else {
-        return await context.api.forwardMessage(environmentConfig.CHANNEL_ID, context.chat.id, context.msg.reply_to_message?.message_id || 0);
-    }
+    if(context.msg?.reply_to_message !== undefined) {
+        return context.api.forwardMessage(environmentConfig.CHANNEL_ID, environmentConfig.CHAT_ID, context.msg.reply_to_message?.message_id || 0);
+    } 
+    return await context.reply('Ви не виділили повідомлення');
 })
