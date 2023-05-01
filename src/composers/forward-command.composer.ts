d9d4c8099d4595f2032649ebb632d0a7489221c5
Arconiel <Arconiel456@gmail.com>
2023-05-01 11:55:15 +0300

    style: fix lint error

--- src/composers/forward-command.composer.ts
+++ src/composers/forward-command.composer.ts
@@ -1,12 +1,17 @@
 import { Composer } from 'grammy';
+
 import { environmentConfig } from '../config';
-import { unCorrectUseForwardCommand } from "../messages";
+import { unCorrectUseForwardCommand } from '../messages';
 
 export const forwardCommandComposer = new Composer();
 
-forwardCommandComposer.command('forward',  async (context)=> {
-    if(context.msg?.reply_to_message) {
-        return context.api.forwardMessage(environmentConfig.CHANNEL_ID, environmentConfig.CHAT_ID, context.msg.reply_to_message?.message_id || 0);
-    }
-    return await context.reply(unCorrectUseForwardCommand);
-})
+forwardCommandComposer.command('forward', async (context) => {
+  if (context.msg?.reply_to_message) {
+    return context.api.forwardMessage(
+      environmentConfig.CHANNEL_ID,
+      environmentConfig.CHAT_ID,
+      context.msg.reply_to_message?.message_id || 0,
+    );
+  }
+  return context.reply(unCorrectUseForwardCommand);
+});
