5c8cbfd0025b78bb7cd98cf7a519b537a866eca7
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2023-05-01 12:20:10 +0300

    fix: express server for long-polling

--- src/bot.server.ts
+++ src/bot.server.ts
@@ -1,9 +1,17 @@
 import express from 'express';
 
+import { environmentConfig } from './config';
+
 export const runBotExpressServer = () => {
   const app = express();
 
   app.get('/health-check', (_request, response) => response.json({ status: 'ok' }));
 
-  return app;
+  const runExpressApp = () => {
+    app.listen(environmentConfig.PORT, () => {
+      console.info(`Bot-server started on http://localhost:${environmentConfig.PORT}`);
+    });
+  };
+
+  return { expressApp: app, runExpressApp };
 };
