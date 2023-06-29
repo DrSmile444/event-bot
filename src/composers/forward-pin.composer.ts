d0a3824f334d02b723167f6ba7731d9cbf43a439
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2023-06-29 13:22:28 +0300

    fix: twice forwarding bug

--- src/composers/forward-pin.composer.ts
+++ src/composers/forward-pin.composer.ts
@@ -8,5 +8,9 @@ import { ignoreOld } from '../middlewares';
 export const forwardPinComposer = new Composer<GrammyContext>();
 
 forwardPinComposer.on(':pinned_message', ignoreOld(ignoredOldMessage), async (context) => {
+  if (context.from?.is_bot) {
+    return;
+  }
+
   await context.api.forwardMessage(environmentConfig.CHANNEL_ID, context.chat.id || 0, context.msg.pinned_message.message_id);
 });
